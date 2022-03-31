cd449bf01ad97a61b0a5ee828f438b9ac12e2ccc
musebe <emusebe9@gmail.com>
2022-03-31 23:08:17 +0300

    added FeedbackList & Item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import{useState} from 'react';
+
+import FeedbackList from './components/FeedbackList';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
 
 const App = () => {
-  return (
-    <h1>Hello from App</h1>
-  )
-}
 
-export default App
+  const [feedback, setFeedback] = useState(FeedbackData);
+  return (
+    <>
+      <Header />
+      <div className='container'>
+
+
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
+  );
+};
+
+export default App;
